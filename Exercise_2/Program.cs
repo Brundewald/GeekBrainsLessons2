3bc6461216f9b508170c03ea5659825f2a94a6de
brundewald <nikyiro@gmail.com>
2021-03-15 14:51:22 +0300

    Выполнено упражнение 2 и 3.

--- Exercise_2/Program.cs
+++ Exercise_2/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 
+/*Created by Nikita Mialkin for GeekBrains*/
+
 namespace Exercise_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi! This program can count digits in your number. Let's try it!\nPlease enter the number.\n");
+            string number = Console.ReadLine();
+            int count = NewMethod(number);
+            Console.WriteLine($"The amount of digits is: {count}");
+
+
+        }
+
+        private static int NewMethod(string number)
+        {
+            int count = 0;
+            foreach (char ch in number)
+            {
+                for (int l = 0; number.Length > l; l++)
+                {
+                    if (ch == number[l]) count++;
+                }
+            }
+
+            return count;
         }
     }
 }
